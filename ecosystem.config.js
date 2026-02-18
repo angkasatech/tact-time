ab7723b211dfb3871c4d528a7202f06fbc53d98a
angkasatech <ramadhanadiputra1@gmail.com>
2026-02-18 07:59:36 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
-{
-    "apps": [{
-        "name": "tact-time",
-        "script": "server.js",
-        "instances": 1,
-        "exec_mode": "cluster",
-        "env": {
+module.exports = {
+    apps: [{
+        name: "tact-time",
+        script: "server.js",
+        instances: 1,
+        exec_mode: "cluster",
+        env: {
             "NODE_ENV": "production",
             "PORT": 8080
         },
-        "error_file": "logs/err.log",
-        "out_file": "logs/out.log",
-        "log_date_format": "YYYY-MM-DD HH:mm:ss Z",
-        "merge_logs": true,
-        "autorestart": true,
-        "max_restarts": 10,
-        "min_uptime": "10s"
+        error_file: "logs/err.log",
+        out_file: "logs/out.log",
+        log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+        merge_logs: true,
+        autorestart: true,
+        max_restarts: 10,
+        min_uptime: "10s"
     }]
-}
+};
